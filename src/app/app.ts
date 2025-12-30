92a8c80779720615cd2310a82edb31ec5870d3be
Vũ Văn Minh <min6th@gmail.com>
2025-12-30 17:32:46 +0700

    feat: implement loading indicators in app component and remove from auth component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
+import { LoadingService } from '../core/services/loading';
+import { ProgressBarModule } from 'primeng/progressbar';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterModule, ToastModule],
+  imports: [CommonModule, RouterModule, ToastModule, ProgressBarModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
   providers: [MessageService],
 })
 export class App {
-  protected readonly title = signal('open-erp-web');
+  private loadingService = inject(LoadingService);
+  private cdr = inject(ChangeDetectorRef);
+
+  loading: boolean = false;
+
+  ngOnInit(): void {
+    this.loadingService.onLoading$().subscribe((loading) => {
+      this.loading = loading;
+      this.cdr.markForCheck();
+    });
+  }
 }
--- src/app/public/auth/auth.ts
+++ src/app/public/auth/auth.ts
@@ -1,29 +1,11 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  inject,
-  type OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProgressBarModule } from 'primeng/progressbar';
-import { LoadingService } from '../../../core/services/loading';
 @Component({
   selector: 'app-auth',
-  imports: [RouterOutlet, ProgressBarModule],
+  imports: [RouterOutlet],
   templateUrl: './auth.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Auth implements OnInit {
-  private loadingService = inject(LoadingService);
-  private cdr = inject(ChangeDetectorRef);
-
-  loading: boolean = false;
-
-  ngOnInit(): void {
-    this.loadingService.onLoading$().subscribe((loading) => {
-      this.loading = loading;
-      this.cdr.markForCheck();
-    });
-  }
+  ngOnInit(): void {}
 }
