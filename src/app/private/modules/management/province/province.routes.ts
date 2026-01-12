63c453bab22ba1447b19543675ddec8a45b0ab58
Vũ Văn Minh <min6th@gmail.com>
2026-01-12 13:36:56 +0700

    Update pagination options and default redirect for province management

--- src/app/private/modules/management/province/province.routes.ts
+++ src/app/private/modules/management/province/province.routes.ts
@@ -13,7 +13,7 @@ export const routes: Routes = [
       {
         path: '',
         pathMatch: 'full',
-        redirectTo: 'all/1/10',
+        redirectTo: 'all/1/100',
       },
       {
         path: ':filter',
