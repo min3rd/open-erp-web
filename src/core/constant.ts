b0da7c2b7deac7c3043670a2fbe6c127c78a8d15
Vũ Văn Minh <min6th@gmail.com>
2026-01-19 09:52:07 +0700

    Update API URI constants for inventory and common services

--- src/core/constant.ts
+++ src/core/constant.ts
@@ -3,7 +3,8 @@ export const API_URI_USER = 'http://localhost:3002';
 export const API_URI_NOTIFICATION = 'http://localhost:3003';
 export const API_URI_CONFIG = 'http://localhost:3004';
 export const API_URI_ORGANIZATION = 'http://localhost:3005';
-export const API_URI_COMMON = 'http://localhost:3006';
+export const API_URI_INVENTORY = 'http://localhost:3006';
+export const API_URI_COMMON = 'http://localhost:3007';
 
 // Pagination constants
 export const PAGE_SIZE_OPTIONS = [100, 500, 1000, 10000];
